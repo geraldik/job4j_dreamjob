2d5b83f7324a5080d89e6311d1066cec61b4e47d
geraldik <geraldik1986@gmail.com>
2022-07-12 23:23:55 +0300

    1. Слоеная архитектура. [#504851]

---
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -0,0 +1,36 @@
+package ru.job4j.dreamjob.service;
+
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.store.PostStore;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PostService {
+
+    private static final PostService  INSTANCE = new PostService();
+    private final PostStore store = PostStore.instOf();
+
+    private PostService() {
+    }
+
+    public static PostService instOf() {
+        return INSTANCE;
+    }
+
+    public Collection<Post> findAll() {
+        return store.findAll();
+    }
+
+    public void add(Post post) {
+        store.add(post);
+    }
+
+    public Post findById(int id) {
+        return store.findById(id);
+    }
+
+    public void update(Post post) {
+        store.update(post);
+    }
+}
