9ded9963a368ecdf467b0c22886bd6b19ec1c3a2
geraldik <geraldik1986@gmail.com>
2022-07-17 22:43:40 +0300

    2. Формы. Списки. [#504854 #322049]

--- src/main/java/ru/job4j/dreamjob/model/City.java
+++ src/main/java/ru/job4j/dreamjob/model/City.java
@@ -49,8 +49,6 @@ public class City {
 
     @Override
     public String toString() {
-        return "City{"
-                + "name='" + id + '\''
-                + '}';
+        return name;
     }
 }
