6f182ed024a48575d42ec6136abf61a76b9aca6d
geraldik <geraldik1986@gmail.com>
2022-07-21 17:01:44 +0300

    3. Тестирование базы данных. Liquibase H2 [#504862 #324491]

--- src/test/java/ru/job4j/dreamjob/store/CandidateStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/store/CandidateStoreTest.java
@@ -1,9 +1,47 @@
 package ru.job4j.dreamjob.store;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.Main;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDate;
+import java.util.List;
 
 class CandidateStoreTest {
+    @Test
+    public void whenCreateCandidate() {
+        CandidateDBStore store = new CandidateDBStore((new Main().loadPool()));
+        Candidate candidate = new Candidate(0, "John",
+                "description", LocalDate.now(), new byte[]{0});
+        store.add(candidate);
+        Candidate postInDb = store.findById(candidate.getId());
+        assertThat(postInDb.getName()).isEqualTo(candidate.getName());
+    }
 
+    @Test
+    public void whenUpdateCandidate() {
+        CandidateDBStore store = new CandidateDBStore((new Main().loadPool()));
+        Candidate candidate = new Candidate(0, "John",
+                "description", LocalDate.now(), new byte[]{0});
+        store.add(candidate);
+        candidate.setName("Bob");
+        store.update(candidate);
+        Candidate postInDb = store.findById(candidate.getId());
+        assertThat(postInDb.getName()).isEqualTo(candidate.getName());
+    }
+
+    @Test
+    public void whenAddTwoCandidatesThenGetAll() {
+        CandidateDBStore store = new CandidateDBStore((new Main().loadPool()));
+        Candidate candidate1 =  new Candidate(0, "John",
+                "description", LocalDate.now(), new byte[]{0});
+        Candidate candidate2 =  new Candidate(0, "Bob",
+                "description", LocalDate.now(), new byte[]{0});
+        store.add(candidate1);
+        store.add(candidate2);
+        List<Candidate> candidates = store.findAll();
+        assertThat(candidates).contains(candidate1, candidate2);
+    }
 }
--- src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
@@ -1,18 +1,43 @@
 package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.Main;
+import ru.job4j.dreamjob.model.City;
 import ru.job4j.dreamjob.model.Post;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 class PostDBStoreTest {
     @Test
     public void whenCreatePost() {
         PostDBStore store = new PostDBStore(new Main().loadPool());
-        Post post = new Post(0, "Java Job", "description");
+        Post post = new Post(0, "Java Job", "description", true, new City());
         store.add(post);
         Post postInDb = store.findById(post.getId());
         assertThat(postInDb.getName()).isEqualTo(post.getName());
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(0, "Java Job", "description", true, new City());
+        store.add(post);
+        post.setName("New name");
+        store.update(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName()).isEqualTo(post.getName());
+    }
+
+    @Test
+    public void whenAddTwoPostsThenGetAll() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post1 = new Post(0, "Java", "description", true, new City());
+        Post post2 = new Post(1, "Job", "description", true, new City());
+        store.add(post1);
+        store.add(post2);
+        List<Post> posts = store.findAll();
+        assertThat(posts).contains(post1, post2);
     }
 }
