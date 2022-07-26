d253553926561201e177e9d3660091bf6525c0c3
geraldik <geraldik1986@gmail.com>
2022-07-26 13:54:43 +0300

    4. Многопоточность в базе данных [#504860 #326014]

--- src/main/java/ru/job4j/dreamjob/model/User.java
+++ src/main/java/ru/job4j/dreamjob/model/User.java
@@ -53,10 +53,14 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
-        return id == user.id ;
+        return id == user.id;
     }
 
     @Override
